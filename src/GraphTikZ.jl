a954e07522a19b480ac68e83a1dce46eb494ac0b
Joey <joeytindall94@googlemail.com>
2023-03-24 15:00:09 -0400

    Added Translate for Polygon

--- src/GraphTikZ.jl
+++ src/GraphTikZ.jl
@@ -87,170 +87,6 @@ function tikz(
     end
   end
 
-<<<<<<< HEAD
-  to_function(f::Function) = f
-  to_function(x) = Returns(x)
-
-  function tikz(
-    g::AbstractGraph;
-    position=default_position,
-    text=default_text,
-    text_size=default_text_size,
-    shape=default_shape,
-    fill_color=default_fill_color,
-    edge_text=default_text,
-    edge_text_size=default_text_size,
-    edge_shape=default_shape,
-    edge_fill_color=default_fill_color,
-    edge_points=default_edge_points,
-  )
-    kwargs = (; position, text, text_size, shape, fill_color, edge_text, edge_text_size, edge_shape, edge_fill_color, edge_points)
-    kwargs = map(to_function, kwargs)
-    (; position, text, text_size, shape, fill_color, edge_text, edge_text_size, edge_shape, edge_fill_color, edge_points) = kwargs
-    dg = DataGraph(g)
-    for v in vertices(dg)
-      v_data = Dict()
-      v_data[:position] = position(v)
-      v_data[:shape] = shape(v)
-      v_data[:text] = text(v)
-      v_data[:text_size] = text_size(v)
-      v_data[:fill_color] = fill_color(v)
-      dg[v] = v_data
-    end
-    for e in edges(dg)
-      e_data = Dict()
-      ## e_data[:position] = edge_position(v)
-      e_data[:edge_shape] = edge_shape(e)
-      e_data[:edge_text] = edge_text(e)
-      e_data[:edge_text_size] = edge_text_size(e)
-      e_data[:edge_fill_color] = edge_fill_color(e)
-      e_data[:edge_points] = edge_points(e)
-      dg[e] = e_data
-    end
-    return tikz(dg)
-  end
-
-  tikz(; kwargs...) = tikz(Graph(1); kwargs...)
-
-  function tikz(
-    shapes::Vector;
-    kwargs...,
-  )
-    return tikz(; shape=shapes, kwargs...)
-  end
-
-  # GeometryBasics.jl extensions
-  # Transformations: translation, rotation, reflection, scaling.
-  # https://github.com/JuliaGeometry/Rotations.jl
-  # https://github.com/JuliaGeometry/CoordinateTransformations.jl
-  # https://discourse.julialang.org/t/non-affine-transformations-in-makie/81951
-  # https://discourse.julialang.org/t/using-arbitrary-polygons-as-markers-in-makie-jl/53690
-  # https://discourse.julialang.org/t/nonorthogonal-axes-in-makie-or-maybe-in-other-plotting-package/86749
-  translate(s, translation) = s + translation
-  function translate(s::LineString, translation)
-    return LineString(coordinates(s) .+ translation)
-  end
-  function translate(s::Line, translation)
-    return Line((coordinates(s) .+ translation)...)
-  end
-  function translate(s::Vec, translation)
-    return Line(translation, Point(s) + translation)
-  end
-  function translate(s::Circle, translation)
-    return Circle(s.center + translation, s.r)
-  end
-  function translate(s::Polygon, translation::Point2)
-    return Polygon(coordinates(s) .+ translation)
-  end
-
-  # GeometryBasics TikZ conversion
-  # https://www.overleaf.com/learn/latex/TikZ_package
-  # https://tikz.dev/tikz-shapes
-  # https://tex.stackexchange.com/questions/107057/adjusting-font-size-with-tikz-picture
-  # \tiny
-  # \scriptsize
-  # \footnotesize
-  # \small
-  # \normalsize
-  # \large
-  # \Large
-  # \LARGE
-  # \huge
-  # \Huge
-  function tikz(s::Point; text="", fill_color=default_fill_color(), text_size=default_text_size())
-    return L"\draw %$(string(Tuple(s))) node {%$(text_size) %$(text)};"
-  end
-
-  function tikz(
-    s::Line{2};
-    line_thickness=default_line_thickness(),
-    line_color=default_line_color(),
-    fill_color=default_fill_color(),
-  )
-    draw_args = "[draw=$(line_color),$(line_thickness)]"
-    return L"\draw%$(draw_args) %$(string(Tuple(s[1]))) -- %$(string(Tuple((s[2]))));"
-  end
-
-  tikz_shape(::Circle) = "circle"
-  function tikz(
-    s::Circle;
-    line_thickness=default_line_thickness(),
-    line_color=default_line_color(),
-    fill_color=default_fill_color(),
-  )
-    filldraw_args = "[draw=$(line_color),$(line_thickness),fill=$(fill_color)!40]"
-    tikz_str = L"\filldraw%$(filldraw_args) %$(string(Tuple(s.center))) %$(tikz_shape(s)) (%$(s.r)) node {};"
-    return tikz_str
-  end
-
-  tikz_shape(::Rect2) = "rectangle"
-  function tikz(
-    s::Rect;
-    line_thickness=default_line_thickness(),
-    line_color=default_line_color(),
-    fill_color=default_fill_color(),
-  )
-    filldraw_args = "[draw=$(line_color),rounded corners,$(line_thickness),fill=$(fill_color)!40]"
-    tikz_str = L"\filldraw%$(filldraw_args) %$(string(Tuple(s.origin))) %$(tikz_shape(s)) %$(string(Tuple(s.origin + s.widths))) node {};"
-    return tikz_str
-  end
-
-  # https://github.com/JuliaGeometry/GeometryBasics.jl/pull/73
-  # `decompose(Point, s)` or `coordinates(s)` returns the points composing the `LineString`.
-  lines(s::LineString) = decompose(Line, s)
-  function tikz(
-    s::LineString;
-    line_thickness=default_line_thickness(),
-    line_color=default_line_color(),
-    fill_color=default_fill_color(),
-    rounded_corners=true,
-  )
-    filldraw_args = "[draw=$(line_color),rounded corners,$(line_thickness)]"
-    tikz_str = "\\draw$(filldraw_args) "
-    for p in coordinates(s)
-      tikz_str *= "$(string(Tuple(p))) -- "
-    end
-    tikz_str = chop(tikz_str; tail=4)
-    tikz_str *= ";"
-    return tikz_str
-  end
-
-  # Always makes a closed shape, use `LineString` to make connected lines.
-  function tikz(
-    s::Polygon;
-    line_thickness=default_line_thickness(),
-    line_color=default_line_color(),
-    fill_color=default_fill_color(),
-  )
-    filldraw_args = "[draw=$(line_color),rounded corners,$(line_thickness),fill=$(fill_color)!40]"
-    tikz_str = "\\filldraw$(filldraw_args) "
-    for p in coordinates(s)
-      tikz_str *= "$(string(Tuple(p))) -- "
-    end
-    tikz_str *= "cycle;"
-    return tikz_str
-  end
-=======
   return tikz_str
 end
 
@@ -283,6 +119,9 @@ function translate(s::Vec, translation::Point2)
 end
 function translate(s::Circle, translation::Point2)
   return Circle(s.center + translation, s.r)
+end
+function translate(s::Polygon, translation::Point2)
+  return Polygon(coordinates(s) .+ translation)
 end
 
 shape(s) = s
@@ -392,5 +231,4 @@ function tikz(
   tikz_str *= "cycle;"
   return tikz_str
 end
->>>>>>> upstream/main
 end
